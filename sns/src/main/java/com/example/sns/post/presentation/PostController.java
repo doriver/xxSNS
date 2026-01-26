2558434f11bff6d4389453bf7aa4f2b21fc4ae01
DESKTOP-I6L47K7\user <leesang@gro>
2026-01-26 14:27:11 +0900

    좋아요 기능

--- sns/src/main/java/com/example/sns/post/presentation/PostController.java
+++ sns/src/main/java/com/example/sns/post/presentation/PostController.java
@@ -2,6 +2,7 @@ package com.example.sns.post.presentation;
 
 import com.example.sns.common.ApiResponse;
 import com.example.sns.common.argumentResolver.UserInfo;
+import com.example.sns.post.application.LikeService;
 import com.example.sns.post.application.PostService;
 import com.example.sns.post.presentation.dto.PostSaveDTO;
 import jakarta.validation.Valid;
@@ -9,10 +10,9 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.HashMap;
 import java.util.Map;
 
 @Controller
@@ -20,6 +20,7 @@ import java.util.Map;
 public class PostController {
 
     private final PostService postService;
+    private final LikeService likeService;
 
     // 게시글 작성
     @PostMapping("/posts")
@@ -33,5 +34,24 @@ public class PostController {
         Long savedPostId = postService.addPost(userId, username, form.getContent(), form.getFile());
 
         return ApiResponse.success();
+    }
+
+    // 게시글 좋아요 + 좋아요 취소
+    @GetMapping("/likes/{postId}")
+    public ApiResponse<Map<String, Object>> likePost(
+            @PathVariable("postId") Long postId
+            , UserInfo userInfo) {
+
+        Long userId = userInfo.getUserId();
+
+        boolean isLike = likeService.like(postId, userId);
+        int likeCount = likeService.countLike(postId);
+
+        Map<String, Object> result = new HashMap<>();
+
+        result.put("like", isLike);
+        result.put("likeCount", likeCount);
+
+        return ApiResponse.success(result);
     }
 }
